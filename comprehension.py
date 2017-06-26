1d848d1dd3c774b57dff20d80c81b7b4db9c4b61
Henrik Miszoglad <henrik.miszoglad@gmail.com>
2017-06-26 16:42:46 +0200

    added comments to all rows

--- comprehension.py
+++ comprehension.py
@@ -1,33 +1,34 @@
-import random
+import random  # import random module
 
-guessesTaken = 0
+guessesTaken = 0  # assign 0 to guessesTaken variable
 
-print('Hello! What is your name?')
-myName = input()
+print('Hello! What is your name?')  # output the string
+myName = input()  # assign myName to user input
 
-number = random.randint(1, 20)
-print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+number = random.randint(1, 20)  # assign number to a random number between 1 and 20
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')  # output string and the user input
 
-while guessesTaken < 6:
-    print('Take a guess.')
-    guess = input()
-    guess = int(guess)
+while guessesTaken < 6:  # create a loop until 6
+    print('Take a guess.')  # output the string
+    guess = input()  # assign guess to user input
+    guess = int(guess)  # convert user input to integer
 
-    guessesTaken += 1
+    guessesTaken += 1  # increase the loop variable by 1
 
-    if guess < number:
-        print('Your guess is too low.')
+    if guess < number:  # create "if" control flow to decide if the user input is less than the random number
+        print('Your guess is too low.')  # if so, output the string
 
-    if guess > number:
-        print('Your guess is too high.')
+    if guess > number:  # check another parameter if the user input is greater thean the random number
+        print('Your guess is too high.')  # output the string
 
-    if guess == number:
-        break
+    if guess == number:  # check the equality of the user input and the random number
+        break  # in this case, end the loop
 
-if guess == number:
-    guessesTaken = str(guessesTaken)
-    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+if guess == number:  # check the equality of the user input and the random number
+    guessesTaken = str(guessesTaken)  # convert the guessesTaken variable to string
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')  # output the string with
+    #  the user input myName and the variable guessesTaken
 
-if guess != number:
-    number = str(number)
-    print('Nope. The number I was thinking of was ' + number)
+if guess != number:  # if user input does not equals to random number
+    number = str(number)  # convert random number to string
+    print('Nope. The number I was thinking of was ' + number)  # output the string with the random number
